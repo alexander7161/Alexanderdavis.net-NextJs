cc4b71f024d3c8467d3cbc275ddc4fc8fc0ddd60
Alexander Davis <alexanderdavis.net@googlemail.com>
2019-04-27 23:25:42 +0100

    added testing and linting to CI

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,23 @@
+import { CssBaseline } from "@material-ui/core";
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
-import { Provider } from "react-redux";
-import store from "./configureStore";
-import "typeface-roboto";
-import CssBaseline from "@material-ui/core/CssBaseline";
 import ReactGA from "react-ga";
+import { Provider } from "react-redux";
+import "typeface-roboto";
+import App from "./App";
+import store from "./configureStore";
+import registerServiceWorker from "./registerServiceWorker";
 
 ReactGA.initialize("UA-44297367-1");
 
 ReactDOM.render(
-	<Provider store={store}>
-		<React.Fragment>
-			<CssBaseline />
-			<App />
-		</React.Fragment>
-	</Provider>,
-	document.getElementById("root")
+    <Provider store={store}>
+        <React.Fragment>
+            <CssBaseline />
+            <App />
+        </React.Fragment>
+    </Provider>,
+    document.getElementById("root"),
 );
 
 registerServiceWorker();
