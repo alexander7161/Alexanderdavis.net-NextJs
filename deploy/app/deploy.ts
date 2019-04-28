a08b5a37c3a941082d1b43f58b7eedc61e58590f
Alexander Davis <alexanderdavis.net@googlemail.com>
2019-04-28 23:39:18 +0100

    update deploy to use FTP_USER

--- deploy/app/deploy.ts
+++ deploy/app/deploy.ts
@@ -4,20 +4,20 @@ import path from "path";
 deploy();
 
 async function deploy() {
-	const client = new Client();
-	client.ftp.verbose = true;
-	try {
-		const { HOST, USER, PASSWORD, HOST_PATH } = process.env;
-		await client.access({
-			host: HOST,
-			user: USER,
-			password: PASSWORD
-		});
-		await client.cd(HOST_PATH!);
-		await client.uploadDir(path.join(__dirname, "../..", "build"), "website");
-	} catch (err) {
-		console.log(err);
-		process.exit(1);
-	}
-	client.close();
+    const client = new Client();
+    client.ftp.verbose = true;
+    try {
+        const { HOST, FTP_USER, PASSWORD, HOST_PATH } = process.env;
+        await client.access({
+            host: HOST,
+            user: FTP_USER,
+            password: PASSWORD,
+        });
+        await client.cd(HOST_PATH!);
+        await client.uploadDir(path.join(__dirname, "../..", "build"), "website");
+    } catch (err) {
+        console.log(err);
+        process.exit(1);
+    }
+    client.close();
 }
