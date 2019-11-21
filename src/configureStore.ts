66e04936b38e1338eb53783dbc004b5f560db10e
Alexander Davis <alexanderdavis.net@googlemail.com>
2019-11-21 12:30:22 +0000

    Convert projects store to use createSlice

--- src/configureStore.ts
+++ src/configureStore.ts
@@ -1,14 +1,14 @@
 import { createBrowserHistory } from "history";
 import { applyMiddleware, combineReducers, compose, createStore } from "redux";
 import thunk from "redux-thunk";
-import projectsReducer from "./pages/projects/store/reducer";
+import projectsReducer from "./pages/projects/store";
 
 const history = createBrowserHistory();
 const composeEnhancers =
-    (window && (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+  (window && (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
 const reducers = combineReducers({
-    projects: projectsReducer,
+  projects: projectsReducer,
 });
 
 export type AppState = ReturnType<typeof reducers>;
