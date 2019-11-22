27749e11425fc6f8f226cb252eb60bd2b3683a0e
Alexander Davis <alexanderdavis.net@googlemail.com>
2019-11-22 10:53:26 +0000

    Fix linting for types

--- src/types/Project.d.ts
+++ src/types/Project.d.ts
@@ -1,15 +1,15 @@
 interface Project extends ProjectFromAPI {
-    lastUpdatedString?: string;
+	lastUpdatedString?: string;
 }
 
 interface ProjectFromAPI {
-    title: string;
-    img: string;
-    description: string;
-    languages: string;
-    siteURL?: string;
-    dateCreated: string;
-    githubURL?: string;
-    downloadURL?: string;
-    lastUpdated?: Date;
+	title: string;
+	img: string;
+	description: string;
+	languages: string;
+	siteURL?: string;
+	dateCreated: string;
+	githubURL?: string;
+	downloadURL?: string;
+	lastUpdated?: Date;
 }
