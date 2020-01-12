8a029aad2a9899698e4d7e99a1af6ffd9135e6d8
Alexander Davis <alexanderdavis.net@googlemail.com>
2020-01-12 23:48:08 +0000

    fix build

--- src/pages/projects/CardButtons.tsx
+++ src/pages/projects/CardButtons.tsx
@@ -1,43 +1,43 @@
+import Button from "./Button";
 import React from "react";
 import styled from "styled-components";
-import Button from "./Button";
 
 const Container = styled.div`
-  margin: 0px 8px;
-  display: flex;
-  @media (max-width: 1279.95px) {
-    display: block;
-  }
+	margin: 0px 8px;
+	display: flex;
+	@media (max-width: 1279.95px) {
+		display: block;
+	}
 `;
 
 export default ({ project }: { project: Project }) => {
-  const { title, githubURL, siteURL, downloadURL, languages } = project;
-  return (
-	<Container>
-		{githubURL && (
-		<Button
-			text="CHECK IT OUT"
-			name="github"
-			URL={githubURL}
-			eventLabel={title + " github"}
-		/>
-		)}
-		{siteURL && (
-		<Button
-			text="VISIT SITE"
-			name="chrome"
-			URL={siteURL}
-			eventLabel={title + " Website"}
-		/>
-		)}
-		{downloadURL && (
-		<Button
-			text="DOWNLOAD"
-			name={languages === "Android" ? "android" : "download"}
-			URL={downloadURL}
-			eventLabel={title + " download"}
-		/>
-		)}
-	</Container>
-  );
+	const { title, githubURL, siteURL, downloadURL, languages } = project;
+	return (
+		<Container>
+			{githubURL && (
+				<Button
+					text="CHECK IT OUT"
+					name="github"
+					URL={githubURL}
+					eventLabel={title + " github"}
+				/>
+			)}
+			{siteURL && (
+				<Button
+					text="VISIT SITE"
+					name="chrome"
+					URL={siteURL}
+					eventLabel={title + " Website"}
+				/>
+			)}
+			{downloadURL && (
+				<Button
+					text="DOWNLOAD"
+					name={languages === "Android" ? "android" : "download"}
+					URL={downloadURL}
+					eventLabel={title + " download"}
+				/>
+			)}
+		</Container>
+	);
 };
