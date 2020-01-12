d6b1e17d375305348ce0285fc9abe6dbffc86f05
Alexander Davis <alexanderdavis.net@googlemail.com>
2020-01-12 20:48:18 +0000

    Create Hackathon cards

--- src/pages/Resume/Hackathons/HackathonCard.tsx
+++ src/pages/Resume/Hackathons/HackathonCard.tsx
@@ -1,7 +1,31 @@
+import { CardContent, Typography } from "@material-ui/core";
 import React from "react";
+import Card from "../../../components/Card";
 
-const HackathonCard = ({ hackathon }: { hackathon: Hackathon }) => {
-	return <div style={{ height: 200 }}>{hackathon.name}</div>;
+const HackathonCard = ({ hackathon: h }: { hackathon: Hackathon }) => {
+	return (
+		<Card>
+			<CardContent>
+				<div
+					style={{
+						display: "flex",
+						justifyContent: "space-between",
+						alignItems: "flex-start"
+					}}
+				>
+					<Typography gutterBottom={true} variant="h5" component="h2">
+						{h.name}
+					</Typography>
+					<Typography variant="body2" color="textSecondary" component="p">
+						{h.date.toLocaleDateString()}
+					</Typography>
+				</div>
+				<Typography variant="body2" color="textSecondary" component="p">
+					{h.location.city}, {h.location.country}
+				</Typography>
+			</CardContent>
+		</Card>
+	);
 };
 
 export default HackathonCard;
