c6535823b0f4623b65fefc12b79298d3703f95e4
Alexander Davis <alexanderdavis.net@googlemail.com>
2020-09-28 23:14:12 +0200

    add test

---
+++ src/pages/Resume/Resume.test.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import { render } from "../../test-utils"
+import Resume from "."
+
+describe("Resume", ()=> {
+    test("snapshot", ()=>{
+        const {asFragment} = render(<Resume  />)
+        expect(asFragment()).toMatchSnapshot()
+    })
+})
