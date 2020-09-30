f88bc0962734180a6fe172d7418d03895e30849d
Alexander Davis <alexanderdavis.net@googlemail.com>
2020-09-30 22:16:15 +0200

    add queries

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,20 @@
 import * as functions from "firebase-functions";
+import admin = require("firebase-admin");
+import { firestore } from "firebase-admin";
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-	functions.logger.info("Hello logs!", { structuredData: true });
-	response.send("Hello from Firebase!");
+admin.initializeApp();
+
+const getCollection = async (collection: string) => {
+	const hackathonData = await firestore().collection(collection).get();
+	return hackathonData.docs.map((d) => d.data());
+};
+
+export const hackathons = functions.https.onRequest(
+	async (request, response) => {
+		response.send(JSON.stringify(await getCollection("hackathons")));
+	}
+);
+
+export const projects = functions.https.onRequest(async (request, response) => {
+	response.send(JSON.stringify(await getCollection("projects")));
 });
